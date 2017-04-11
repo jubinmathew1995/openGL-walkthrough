c0cf635ce7ec56bf7d9226c288a02d2c5ca9172c
Jubin Mathew <jubinmathew1995@gmail.com>
2017-04-11 22:17:35 +0530

    show the use of mouse and keyboard in the glut window and log there activity in the console.

---
+++ program03.cpp
@@ -0,0 +1,145 @@
+/*
+
+CPP Program to make a graphic window
+using openGL graphics library.
+
+Program to show the use of mouse and keyboard
+and logging their states.
+
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <GL/glut.h>
+
+// window TITLE, WIDTH and HEIGHT
+#define WINDOW_TITLE_PREFIX ""
+int windowWidth = 800,
+    windowHeight = 600;
+
+// function prototype
+static void ResizeFunction(int, int);
+static void displayFunction(void);
+static void idleFunction(void);
+static void keyFunction(unsigned char, int, int);
+static void mouseMove(int, int);
+static void mouseButton(int, int, int, int);
+
+int main(int argc, char *argv[])
+{
+    // init GLUT and create Window
+    glutInit(&argc, argv);
+    glutInitWindowSize(windowWidth,windowHeight);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow(WINDOW_TITLE_PREFIX);
+
+    // setting correct perspective when window size is changed.
+    glutReshapeFunc(ResizeFunction);
+
+    // register callbacks
+    glutDisplayFunc(displayFunction);
+
+    // keyFunction is called whenever an keyboard event is encountered.
+    glutKeyboardFunc(keyFunction);
+
+    // this IDLE tells the glut to keep calling render function
+    // to allow animations.
+    glutIdleFunc(idleFunction);
+
+    // sets the glut to listen to the mouse actions.
+    glutMouseFunc(mouseButton);
+    glutMotionFunc(mouseMove);
+
+    // sets the background color RED:GREEN:BLUE:ALPHA
+    glClearColor(0,0,0,0);
+
+    // enter GLUT event processing cycle/
+    glutMainLoop();
+
+    return 1;
+}
+
+static void ResizeFunction(int width, int height)
+{
+    windowHeight=height;
+    windowWidth=width;
+    glViewport(0, 0, width, height);
+}
+
+static void displayFunction(void)
+{
+    // clear previous colors.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glVertex3f(-0.5,-0.5,0.0);
+        glVertex3f(0.5,0.0,0.0);
+        glVertex3f(0.0,0.5,0.0);
+    glEnd();
+
+    // swaps the front and back buffers.
+    glutSwapBuffers();
+}
+
+
+static void idleFunction(void)
+{
+    // tells GULT to continously redisplay/call the
+    // display function continously, so that animations
+    // can be possible.
+    glutPostRedisplay();
+}
+
+static void keyFunction(unsigned char key, int x, int y)
+{
+    printf("keyboard--%d->%c\n",key,char(key));
+    switch (key)
+    {
+        case 27 :
+        case 'q':
+            printf("quit\n");
+            exit(0);
+            break;
+    }
+    glutPostRedisplay();
+}
+
+static void mouseMove(int x, int y) {
+    printf("Mouse-Coodinates:(%d,%d)\n",x,y );
+}
+
+static void mouseButton(int button, int state, int x, int y) {
+
+	// only start motion if the left button is pressed
+	if (button == GLUT_LEFT_BUTTON) {
+
+		// when the button is released
+		if (state == GLUT_UP) {
+			printf("Mouse--LEFT--onKeyUp--(%d,%d)\n",x,y );
+		}
+		else  {// state = GLUT_DOWN
+            printf("Mouse--LEFT--onKeyDown--(%d,%d)\n",x,y );
+		}
+	}
+    else if (button == GLUT_RIGHT_BUTTON) {
+
+        // when the button is released
+        if (state == GLUT_UP) {
+            printf("Mouse--RIGHT--onKeyUp--(%d,%d)\n",x,y );
+        }
+        else  {// state = GLUT_DOWN
+            printf("Mouse--RIGHT--onKeyDown--(%d,%d)\n",x,y );
+        }
+    }
+    else {
+
+        // when the button is released
+        if (state == GLUT_UP) {
+            printf("Mouse--MIDDLE--onKeyUp--(%d,%d)\n",x,y );
+        }
+        else  {// state = GLUT_DOWN
+            printf("Mouse--MIDDLE--onKeyDown--(%d,%d)\n",x,y );
+        }
+    }
+}
